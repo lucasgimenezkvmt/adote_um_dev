6a976b1448d99ab4bf7299e02cd57b02b6eb37af
lucasgimenezkvmt <129784127+lucasgimenezkvmt@users.noreply.github.com>
2025-06-02 18:08:33 -0300

    Envio de componentes

---
+++ app/Http/Livewire/Components/Splash/Screen.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Components\Splash;
+
+use Livewire\Component;
+
+class Screen extends Component
+{
+    public function render()
+    {
+        return view('livewire.components.splash.screen');
+    }
+}
