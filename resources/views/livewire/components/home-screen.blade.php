0947696ffe256cd9b73fb67dc2d999f57122ebe7
Lucas Dias <lucas_d360@hotmail.com>
2025-06-05 20:04:07 -0300

    melhorias

--- resources/views/livewire/components/home-screen.blade.php
+++ resources/views/livewire/components/home-screen.blade.php
@@ -18,14 +18,14 @@
             <button wire:click="loginWithGoogle"
                 class="flex flex-row space-x-2 justify-center items-center bg-white p-4 text-gray-100 w-full text-sm rounded-full mt-8 font-bold transform duration-150 active:scale-95"
             >
-                {{--<x-icon-google class="h-4 w-4 fill-current" />--}}
+                <x-icon-google class="h-4 w-4 fill-current" />
                 <span>Entrar como Recrutador</span>
             </button>
 
             <button wire:click="loginWithGithub"
-                class="flex flex-row space-x-2 justify-center items-center border-white border-2 border-solid p-4 text-white mb-8 mt-4 w-full text-sm rounded-full font-bold transform duration-150 active:scale-95"
+                class="flex flex-row space-x-2 justify-center items-center border-white border-2 border-solid p-4 text-black mb-8 mt-4 w-full text-sm rounded-full font-bold transform duration-150 active:scale-95"
             >
-                {{--<x-icon-github class="w-4 h-4 fill-current" />--}}
+                <x-icon-github class="w-4 h-4 fill-current" />
                 <span>Entrar como Dev</span>
             </button>
 
